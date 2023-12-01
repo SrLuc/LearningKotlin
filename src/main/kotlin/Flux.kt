fe000b7d95f76644e429b4ff65de71fb01d15f15
SrLuc <ruanlucenaramos@gmail.com>
2023-12-01 17:02:25 -0300

    Learnign about Flux, it is kind of Switch case in js Language

--- src/main/kotlin/Flux.kt
+++ src/main/kotlin/Flux.kt
@@ -29,6 +29,28 @@ fun userData( datas: Any){
 
 }
 
+fun weekDay(day:Any){
+    when(day){
+        1 -> println("Monday")
+        2 -> println("Tuesday")
+        3 -> println("Wednesday")
+        4 -> println("Thursday")
+        5 -> println("Friday")
+        6 -> println("Saturday")
+        7 -> println("Sunday")
+        else -> println("Not assingment")
+    }
+}
+
+fun typeOf(data:Any){
+    when(data){
+        is String -> println("String Data")
+        is Int -> println("Type Number Data")
+        is Boolean -> println("Type Boolean Data")
+        else -> println("Not Recognized Data")
+    }
+}
+
 fun main(){
 
 }
